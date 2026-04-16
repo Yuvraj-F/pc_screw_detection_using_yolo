c9a05f32cd73b575f157a1935fb3778c0ae93bfc
Yuvraj <ys2000126@gmail.com>
2026-04-17 00:25:20 +1200

    Updated train.py to automatically copy the best weights after training to the best weights directory

--- src/train.py
+++ src/train.py
@@ -5,11 +5,17 @@ Date: 16/04/2026
 ChatGPT was used to fix syntax errors and generate boilerplate where needed. Any generated code was used only as reference and edited, refactored, and structured manually unless stated otherwise. 
 """
 
+import shutil
 from pathlib import Path
 from ultralytics import YOLO
 
 from config import *
 import model_loader as ml
+
+def copy_best_weights(model_name):
+    weights_path = BEST_WEIGHTS_DIR / model_name
+    weights_path.mkdir(parents=True, exist_ok=True)
+    shutil.copy2(RUNS_DIR / model_name / "weights" / "best.pt", dst=weights_path)
 
 def train_model():
     model = None
@@ -18,7 +24,8 @@ def train_model():
         model_variant = ml.get_model_variant_from_user()
         model = ml.load_model(model_name, model_variant)
 
-    results = model.train(data=DATA_YAML, exist_ok=True, project=RUNS_DIR, name=model_variant)
+    results = model.train(data=DATA_YAML, exist_ok=True, project=RUNS_DIR, name=model_name)
+    copy_best_weights(model_name)
 
 if __name__ == "__main__":
     train_model()
